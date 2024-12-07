157a75a50cd52b557739bd805b2a1beab4c6420a
Joy Ram <joyram2015@gmail.com>
2024-12-07 14:59:51 +0200

    change image property

--- components/details/FoodPoster.jsx
+++ components/details/FoodPoster.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function FoodPoster({ poster }) {
   return (
-    <div className='col-span-12 md:col-span-6'>
+    <div className='col-span-12 md:col-span-6 rounded-lg overflow-hidden'>
       <Image
         src={poster}
         alt='food banner'
--- components/home/FoodCard.jsx
+++ components/home/FoodCard.jsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 export default function FoodCard({ recipe }) {
   const { name, thumbnail, id, rating, author } = recipe || {};
   return (
-    <Link href={`/details/${id}`} className='card'>
-      <Image
-        src={thumbnail}
-        className='rounded-md'
-        alt='foodCard'
-        width={300}
-        height={160}
-      />
+    <Link href={`/details/${id}`} className='card '>
+      <div className='w-[300px] h-[160px] relative rounded-md overflow-hidden'>
+        <Image
+          fill
+          src={thumbnail}
+          className='absolute object-cover object-center'
+          alt='foodCard'
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+        />
+      </div>
       <h4 className='my-2'>{name}</h4>
       <div className='py-2 flex justify-between text-xs text-gray-500'>
         <span>⭐️ {rating}</span>
